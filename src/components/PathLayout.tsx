2e7d368c192025d6a463d5af7d049ea5b224491c
sukhtolawal <sukhwindersidhu81@gmail.com>
2025-05-10 13:40:50 +0530

    minor changes

--- src/components/PathLayout.tsx
+++ src/components/PathLayout.tsx
@@ -2,8 +2,8 @@ import  {  type ReactNode } from 'react'
 
 const PathLayout = ({children}:{children:ReactNode}) => {
   return (
-    <div className='h-screen w-screen flex justify-center px-10 overflow-y-auto'>
-        <div className='mt-20 px-10 py-20 flex flex-col  bg-white/30 shadow-lg rounded-2xl font-bold text-lg gap-20 h-fit'>
+    <div className='h-screen w-screen flex justify-center px-2 overflow-y-auto'>
+        <div className='mt-20 px-5 py-20 flex flex-col  bg-white/30 shadow-lg rounded-2xl font-bold text-lg gap-20 h-fit'>
         
             {children}
                     
