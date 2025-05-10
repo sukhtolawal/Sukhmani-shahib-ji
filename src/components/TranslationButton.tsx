4c4269ffab0bec0d41ff1f19053a7d93289f40db
sukhtolawal <sukhwindersidhu81@gmail.com>
2025-05-10 12:51:49 +0530

    font changes of translation

--- src/components/TranslationButton.tsx
+++ src/components/TranslationButton.tsx
@@ -13,7 +13,7 @@ export const TranslationButton = () => {
   return (
     <div className='flex flex-col items-end fixed bottom-5 right-8 tansition-all duration-300'>
 
-       {togglePopup && <div className='px-4 flex gap-7 bg-white/70 backdrop-blur-3xl w-fit py-2 rounded-3xl mb-3'>Translation <Switch name= {'Translation'} handleToggle = {handleTogglePopup}/> </div>}
+       {togglePopup && <div className='px-4 flex gap-7 bg-white/70 backdrop-blur-3xl w-fit py-2 rounded-3xl mb-3 shadow-2xl'><div className='font-semibold font-sans'>Translation</div> <Switch name= {'Translation'} handleToggle = {handleTogglePopup}/> </div>}
       <button className='bg-white/70 backdrop-blur-3xl rounded-full flex justify-center items-center p-5 shadow-lg cursor-pointer active:bg-white/40 transition-all duration-200 transform active:scale-80' onClick={handleTogglePopup}>{!togglePopup ? <FaPlus size={26} onClick={handleTogglePopup}/> : <FaMinus onClick={handleTogglePopup}/>}</button>
       
       
